441aff271aa8b55b10bacd5230a536431dd10e49
Vaida Juskaite <vaida.juskaite@just-eat.com>
2020-06-20 13:32:20 +0100

    adapts for fork repo

--- dev/01_start.R
+++ dev/01_start.R
@@ -17,8 +17,8 @@ golem::fill_desc(
   pkg_name = "ShinyCICD", # The Name of the package containing the App
   pkg_title = "A Minimal Package for Shiny CI/CD Pipelines", # The Title of the package containing the App
   pkg_description = "A minimal package for Shiny CI/CD pipelines.", # The Description of the package containing the App
-  author_first_name = "Mirai", # Your First Name
-  author_last_name = "Solutions", # Your Last Name
+  author_first_name = "Vaida", # Your First Name
+  author_last_name = "Juskaite", # Your Last Name
   author_email = "info@mirai-solutions.com", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional)
 )
